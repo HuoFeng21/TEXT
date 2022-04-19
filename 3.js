c3a7323923ad17b5b15a8f165c43256c7fe7d387
HuoFeng21 <103394782+HuoFeng21@users.noreply.github.com>
2022-04-19 12:26:42 +0800

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,14 @@
+function myFunction() {
+ var x =  document.getElementById("myText1").value;
+ var y =  document.getElementById("myText2").value;
+ var x =  Number(x)
+ var y =  Number(y)
+ var z = x + y;
+ var greeting;
+ if ( isNaN(z) ){
+   greeting = "error";
+ } else {
+    greeting = z ;
+ }
+  document.getElementById("demo").innerHTML = greeting;
+}
